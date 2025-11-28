82ca34b4d2754dff3b9cdadb67034e327fbdd96d
Saraswathi <saraswathiachari02@gmail.com>
2025-11-28 17:05:10 +0530

    update

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const root = createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <HashRouter>
-  <App />
-</HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
